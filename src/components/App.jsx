a325e4780716f6408319964befe5ee7d02010ecb
Treveon Edwards <fasuke322@gmail.com>
2021-09-10 15:50:23 -0400

    added async promise method fot my music list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,35 @@
 import React, { Component } from "react";
 import './App.css'
+import axios from "axios";
+
+class App extends Component{
+    state = {
+        songs: []
+    }
+
+
+componentDidMount(){
+    this.getAllSongs();
+}
+
+
+
+async getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/my_music_list/')
+    this.setState({
+        songs: response.data
+    });
+    console.console(response.data)
+}
+
+render() {
+    return (
+        <React.Fragment>
+            <h1> log music list</h1>
+            <button onClick ={this.getAllSongs}>test</button>
+        </React.Fragment>
+    );
+    } 
+}
+
+export default App;

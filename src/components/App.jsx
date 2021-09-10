ae2c19396d6353f4ad4e17d2a46de7d47505d36a
Treveon Edwards <fasuke322@gmail.com>
2021-09-10 17:15:58 -0400

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,34 +2,41 @@ import React, { Component } from "react";
 import './App.css'
 import axios from "axios";
 
-class App extends Component{
-    state = {
-        songs: []
+class App  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+            songs: []
+        }
+    
+  
+    
+}
+ 
+
+
+    componentDidMount(){
+        this.getAllSongs();
     }
 
 
-componentDidMount(){
-    this.getAllSongs();
-}
 
+    async getAllSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/my_music_list/');
+        this.setState({
+            songs: response.data
+        });
+        console.log(response.data)
+    }
 
-
-async getAllSongs(){
-    let response = await axios.get('http://127.0.0.1:8000/my_music_list/')
-    this.setState({
-        songs: response.data
-    });
-    console.console(response.data)
-}
-
-render() {
-    return (
-        <React.Fragment>
-            <h1> log music list</h1>
-            <button onClick ={this.getAllSongs}>test</button>
-        </React.Fragment>
-    );
-    } 
-}
+    render() {
+        return (
+            <React.Fragment>
+                <h1>  music list</h1>
+                <button onClick ={this.getAllSongs}>test</button>
+            </React.Fragment>
+        );
+        } 
+    }
 
 export default App;
